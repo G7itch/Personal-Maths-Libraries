5dd405ea2c845a40e2fb8cfb52c5d87d65ca833a
= <radioactivehuman64@gmail.com>
2025-12-02 17:26:12 +0000

    more work

--- C/polymath.c
+++ C/polymath.c
@@ -12,9 +12,21 @@ const char *FieldSymbols[5] = {
 };
 
 
-void* allocate_coeff_memory(int degree, Field coeff_field){
+void* init_allocate_coeff_memory(int degree, Field coeff_field){
     /*
+    Function to allocate the memory for the coefficients, this is variable upon their type and the amount of elements we have
+
+    Parameters
+    ----------
+    degree      :   int
+                    The highest natural power in the polynomial
+    coeff_field :   Field
+                    The field that the polynomial coefficients lie in. Taken from the enum Field, defined in polymath.h
     
+    Returns
+    -------
+    ptr         :   void*
+                    A pointer of any type to the newly allocated memory.
     */
     void* ptr = NULL;
     switch (coeff_field)
@@ -35,13 +47,25 @@ void* allocate_coeff_memory(int degree, Field coeff_field){
 }
 
 void input_coeffs(Polynomial* p){
+    /*
+    Function for when the user wants to change the polynomial coefficients after declaring a new polynomial
+
+    Parameters
+    ----------
+    p   :   Polynomial*
+            A pointer to the polynomial object that you want to change the coefficients for
+
+    Returns
+    -------
+    NULL
+    */
     switch(p->coeff_field)
     {
         case INTEGER:
         case INT_MOD_K:
             printf("\rEnter the integer coefficients with space delimiting: ");
             for (int i=0; i < p->degree; i++){
-                scanf("%d", &((int*)p->coeffs)[i]);
+                scanf("%d", &((int*)p->coeffs)[i]); //Type convert, then dereference, then take the address, then do pointer arithmetic.
             }
             break;
         case COMPLEX:
@@ -57,42 +81,87 @@ void input_coeffs(Polynomial* p){
 }
 
 
-Polynomial init_polynomial(int degree, Field coeff_field){
+Polynomial init_polynomial(char variable, int degree, Field coeff_field, void* coeffs){
     /*
-    
+    Helper function to create a polynomial object, includes basic error handling - needs to be improved
+
+    Parameters
+    ----------
+    variable    :   char
+                    The indeterminate to be used with the polynomial
+    degree      :   degree
+                    The highest natural power of the indeterminate in the polynomial
+    coeff_field :   Field
+                    The field of the cooefficients, taken from an enum
+    coeffs      :   void*
+                    A pointer of any type to a memory location containing the array of coefficients
+
+    Returns
+    -------
+    p           :   Polynomial
+                    The newly initialised polynomial object
     */
     Polynomial p;
     p.degree = degree;
     p.coeff_field = coeff_field;
 
-    p.coeffs = allocate_coeff_memory(degree, coeff_field);
+    p.coeffs = init_allocate_coeff_memory(degree, coeff_field);
     
     if (p.coeffs == NULL){
         fprintf(stderr, "Memory allocation failed!\n");
         exit(1);
     }
-
-    input_coeffs(&p);
+    
+    // Error checking needs to be increased here so that we cant provide less coefficients than degree
+    if (coeff_field == INTEGER || coeff_field == INT_MOD_K) {
+        int* dest = (int*)p.coeffs;
+        int* src  = (int*)coeffs;
+        for (int i = 0; i < degree; i++)
+            dest[i] = src[i];
+    } else { // assume double. Here we have to change this when we add complex type
+        double* dest = (double*)p.coeffs;
+        double* src = (double*)coeffs;
+        for (int i = 0; i < degree; i++)
+            dest[i] = src[i];
+    }
 
     return p;
 }
 
 void free_polynomial(Polynomial *p){
     /*
-    
+    Function to free up the heap allocated memory in the polynomial object. The user has to manually call this
+
+    Parameters
+    ----------
+    p   :   Polynomial*
+            A pointer to a polynomial object in memory. This is a memory address
+
+    Returns
+    -------
+    NULL
     */
     free(p->coeffs); //dereference then free
 }
 
 void print_polynomial(Polynomial *p){
     /*
-    
+    Function to pretty print the polynomial object - needs to add symbolic representation
+
+    Parameters
+    ----------
+    p   :   Polynomial*
+            A pointer to a polynomial object in memory. This is a memory address
+
+    Returns
+    -------
+    NULL
     */
     printf("\n\n----------");
     printf("\nPolynomial");
     printf("\n----------");
     printf("\nDegree: %d", p->degree);
-    printf("\nCoefficient field: %s\n", FieldSymbols[p->coeff_field]);
+    printf("\nCoefficient field: %s", FieldSymbols[p->coeff_field]);
     
     switch(p->coeff_field)
     {
@@ -100,7 +169,7 @@ void print_polynomial(Polynomial *p){
         case INT_MOD_K:
             printf("\nCoefficients: ");
             for (int i=0; i < p->degree; i++){
-                printf("%d ", ((int*)p->coeffs)[i]);
+                printf("%d", ((int*)p->coeffs)[i]);
             }
             break;
         case COMPLEX:
@@ -109,24 +178,19 @@ void print_polynomial(Polynomial *p){
         default:
             printf("\nCoefficients: ");
             for (int i=0; i < p->degree; i++){
-                printf("%.5f ", ((double*)p->coeffs)[i]);
+                printf("%.5f", ((double*)p->coeffs)[i]);
             }
             break;
     }
 }
 
+// Function for testing, will disappear when we make it a library
 int main(){
-    Polynomial test = init_polynomial(2, INTEGER);
-    Polynomial test2 = init_polynomial(3, REAL);
-    Polynomial test3 = init_polynomial(1, INT_MOD_K);
+    Polynomial test = init_polynomial('x', 2, INTEGER, (int[]){3,7});
 
     print_polynomial(&test);
-    print_polynomial(&test2);
-    print_polynomial(&test3);
 
     free_polynomial(&test);
-    free_polynomial(&test2);
-    free_polynomial(&test3);
 
     return 0;
 }
--- C/polymath.h
+++ C/polymath.h
@@ -13,17 +13,25 @@ typedef enum Field {
 extern const char *FieldSymbols[];
 
 typedef struct Polynomial{
+    char variable;
     int degree;    
     void* coeffs;
     Field coeff_field;
 } Polynomial;
 
-void* allocate_coeff_memory();
+void* init_allocate_coeff_memory();
 
 void input_coeffs(Polynomial *p);
 
-Polynomial init_polynomial(int degree, Field coeff_field);
+Polynomial init_polynomial(char variable, int degree, Field coeff_field, void* coeffs);
 
 void free_polynomial(Polynomial *p);
 
 void print_polynomial(Polynomial *p);
+
+/* TO DO */
+// Polynomial add_polynomial(Polynomial *p_1, Polynomial *p_2);
+// Polynomial mul_polynomial(Polynomial *p_1, Polynomial *p_2);
+// Polynomial zero_polynomial();
+// Polynomial scal_mul_polynomial(double lambda, Polynomial *p_1);
+// etc
